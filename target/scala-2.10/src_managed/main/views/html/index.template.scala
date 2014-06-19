18b8994e10213af867514778cce982a47b6fcb7f
WesleyMarques <wesley.torres@ccc.ufcg.edu.br>
2014-06-19 15:13:16 -0300

    integração scala com layout realizado

--- target/scala-2.10/src_managed/main/views/html/index.template.scala
+++ target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -29,34 +29,41 @@ object index extends BaseScalaTemplate[play.api.templates.HtmlFormat.Appendable,
 Seq[Any](format.raw/*1.56*/("""
 
 """),_display_(Seq[Any](/*3.2*/main("Hackfest")/*3.18*/ {_display_(Seq[Any](format.raw/*3.20*/("""
+<meta charset="utf-8">
 <div class="jumbotron">
 	<div class="container">
 		<h1>Hack</h1>
-		<p>Descri��o geral</p>
+		<p>Descrição geral</p>
 		<button class="btn btn-success">Criar Evento</button>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
+	"""),_display_(Seq[Any](/*14.3*/if(hackFest.size() == 0)/*14.27*/{_display_(Seq[Any](format.raw/*14.28*/("""
+	<div class="col-md-12">
+		<div class="alert alert-success">
+		<h2>Não há nenhum evento registrado</h2>
+		</div>
+	</div>
+	
+	""")))}/*21.3*/else/*21.7*/{_display_(Seq[Any](format.raw/*21.8*/("""
+	"""),_display_(Seq[Any](/*22.3*/for(listHack <- hackFest) yield /*22.28*/{_display_(Seq[Any](format.raw/*22.29*/("""
 		<div class="col-md-4">
 			<div class="alert alert-info">
-				<h4>T�tulo</h4>
-				<p>n participantes - <b>DD/MM/AAAA</b></p>
+				<h4>"""),_display_(Seq[Any](/*25.10*/listHack/*25.18*/.getTitleHack())),format.raw/*25.33*/("""</h4>
+				<p>"""),_display_(Seq[Any](/*26.9*/listHack/*26.17*/.getParticipantes().size())),format.raw/*26.43*/(""" participantes - <b>"""),_display_(Seq[Any](/*26.64*/listHack/*26.72*/.getDate())),format.raw/*26.82*/("""</b></p>
 			</div>
-			<p>conteudo</p>
-			<button class="btn btn-info">Mais informa��es</button>
+			<p>"""),_display_(Seq[Any](/*28.8*/listHack/*28.16*/.getDescription())),format.raw/*28.33*/("""</p>
+			<button class="btn btn-info">Mais informações</button>
 			<button class="btn btn-success">Cadastrar-se</button>
 		</div>
-		<div class="col-md-4">teste</div>
-		<div class="col-md-4">teste</div>
-		<div class="col-md-4">test</div>
-
+		""")))}))))})),format.raw/*32.5*/("""
 	</div>
 
 </div>
 
 
-""")))})),format.raw/*31.2*/("""
+""")))})),format.raw/*38.2*/("""
 """))}
     }
     
@@ -69,11 +76,11 @@ Seq[Any](format.raw/*1.56*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Thu Jun 19 14:58:18 BRT 2014
+                    DATE: Thu Jun 19 15:12:09 BRT 2014
                     SOURCE: C:/Users/Wesley/Dropbox/workspaces/workspaceJEE/si1-lab2-P3/app/views/index.scala.html
-                    HASH: 93061abe6d35bf68da33eea5268702eb64384fd0
-                    MATRIX: 799->1|947->55|984->58|1008->74|1047->76|1689->687
-                    LINES: 26->1|29->1|31->3|31->3|31->3|59->31
+                    HASH: 2eaf5826da2bb7b8328f8123da022d7a280a137c
+                    MATRIX: 799->1|947->55|984->58|1008->74|1047->76|1312->306|1345->330|1384->331|1528->457|1540->461|1578->462|1616->465|1657->490|1696->491|1801->560|1818->568|1855->583|1904->597|1921->605|1969->631|2026->652|2043->660|2075->670|2136->696|2153->704|2192->721|2360->854|2411->874
+                    LINES: 26->1|29->1|31->3|31->3|31->3|42->14|42->14|42->14|49->21|49->21|49->21|50->22|50->22|50->22|53->25|53->25|53->25|54->26|54->26|54->26|54->26|54->26|54->26|56->28|56->28|56->28|60->32|66->38
                     -- GENERATED --
                 */
             
--- target/scala-2.10/src_managed/main/views/html/main.template.scala
+++ target/scala-2.10/src_managed/main/views/html/main.template.scala
@@ -33,17 +33,16 @@ Seq[Any](format.raw/*1.32*/("""
 <html>
     <head>
         <title>"""),_display_(Seq[Any](/*7.17*/title)),format.raw/*7.22*/("""</title>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*9.54*/routes/*9.60*/.Assets.at("stylesheets/main.css"))),format.raw/*9.94*/("""">
-        <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*10.59*/routes/*10.65*/.Assets.at("images/favicon.png"))),format.raw/*10.97*/("""">
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*11.54*/routes/*11.60*/.Assets.at("stylesheets/css-bootstrap/bootstrap.min.css"))),format.raw/*11.117*/("""">
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*12.54*/routes/*12.60*/.Assets.at("stylesheets/css-bootstrap/bootstrap-theme.min.css"))),format.raw/*12.123*/("""">
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*13.54*/routes/*13.60*/.Assets.at("stylesheets/css-bootstrap/style.css"))),format.raw/*13.109*/("""">
-        <script src=""""),_display_(Seq[Any](/*14.23*/routes/*14.29*/.Assets.at("javascripts/jquery-1.9.0.min.js"))),format.raw/*14.74*/("""" type="text/javascript"></script>
-        <script src=""""),_display_(Seq[Any](/*15.23*/routes/*15.29*/.Assets.at("javascripts/bootstrap.min.js"))),format.raw/*15.71*/("""" type="text/javascript"></script>
+        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*8.54*/routes/*8.60*/.Assets.at("stylesheets/main.css"))),format.raw/*8.94*/("""">
+        <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*9.59*/routes/*9.65*/.Assets.at("images/favicon.png"))),format.raw/*9.97*/("""">
+        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*10.54*/routes/*10.60*/.Assets.at("stylesheets/css-bootstrap/bootstrap.min.css"))),format.raw/*10.117*/("""">
+        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*11.54*/routes/*11.60*/.Assets.at("stylesheets/css-bootstrap/bootstrap-theme.min.css"))),format.raw/*11.123*/("""">
+        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*12.54*/routes/*12.60*/.Assets.at("stylesheets/css-bootstrap/style.css"))),format.raw/*12.109*/("""">
+        <script src=""""),_display_(Seq[Any](/*13.23*/routes/*13.29*/.Assets.at("javascripts/jquery-1.9.0.min.js"))),format.raw/*13.74*/("""" type="text/javascript"></script>
+        <script src=""""),_display_(Seq[Any](/*14.23*/routes/*14.29*/.Assets.at("javascripts/bootstrap.min.js"))),format.raw/*14.71*/("""" type="text/javascript"></script>
     </head>
     <body>
-        """),_display_(Seq[Any](/*18.10*/content)),format.raw/*18.17*/("""
+        """),_display_(Seq[Any](/*17.10*/content)),format.raw/*17.17*/("""
     </body>
 </html>
 """))}
@@ -58,11 +57,11 @@ Seq[Any](format.raw/*1.32*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Wed Jun 18 18:48:20 BRT 2014
+                    DATE: Thu Jun 19 15:03:51 BRT 2014
                     SOURCE: C:/Users/Wesley/Dropbox/workspaces/workspaceJEE/si1-lab2-P3/app/views/main.scala.html
-                    HASH: 2ae06ec62a8cdd2d7c3d735db1ee2451b51c2398
-                    MATRIX: 778->1|902->31|990->84|1016->89|1189->227|1203->233|1258->267|1355->328|1370->334|1424->366|1516->422|1531->428|1611->485|1703->541|1718->547|1804->610|1896->666|1911->672|1983->721|2044->746|2059->752|2126->797|2219->854|2234->860|2298->902|2401->969|2430->976
-                    LINES: 26->1|29->1|35->7|35->7|37->9|37->9|37->9|38->10|38->10|38->10|39->11|39->11|39->11|40->12|40->12|40->12|41->13|41->13|41->13|42->14|42->14|42->14|43->15|43->15|43->15|46->18|46->18
+                    HASH: 575e5e7a2bfa8960473b3df991c806f48f436e06
+                    MATRIX: 778->1|902->31|990->84|1016->89|1113->151|1127->157|1182->191|1278->252|1292->258|1345->290|1437->346|1452->352|1532->409|1624->465|1639->471|1725->534|1817->590|1832->596|1904->645|1965->670|1980->676|2047->721|2140->778|2155->784|2219->826|2322->893|2351->900
+                    LINES: 26->1|29->1|35->7|35->7|36->8|36->8|36->8|37->9|37->9|37->9|38->10|38->10|38->10|39->11|39->11|39->11|40->12|40->12|40->12|41->13|41->13|41->13|42->14|42->14|42->14|45->17|45->17
                     -- GENERATED --
                 */
             
